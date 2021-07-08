3cf815b6c7fa5de5a1fbcba767a0abe56689a966
Sandman006 <zbn0238@163.com>
2021-07-08 14:30:24 +0800

    change name

---
+++ LED_flash/User/main.c
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"                  // Device header
+#include "led.h"
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+
+int main(void)
+{
+	LED_GPIO_Config();
+	
+	while(1)
+	{
+		GPIO_SetBits(LED_GPIO_PORT, LED_GPIO_PIN);
+		Delay(1000000);
+
+		GPIO_ResetBits(LED_GPIO_PORT, LED_GPIO_PIN);
+		Delay(1000000);
+
+	}
+}
